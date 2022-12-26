5fd15697c7209c05ab1875a7d3eba4d44005ef0a
Wojtas399 <wojtekp26@gmail.com>
2022-12-26 22:51:50 +0100

    Created bottom navigator.

--- app/src/main/java/com/example/restcountriesapp/MainActivity.kt
+++ app/src/main/java/com/example/restcountriesapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.restcountriesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.restcountriesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+  private val navController: NavController by lazy {
+    val navHostFragment =
+      supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    navHostFragment.findNavController()
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
+    binding.bottomNavView.setupWithNavController(navController)
+
+    setContentView(binding.root)
   }
 }
