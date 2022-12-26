53e9ce9a720eb8e03b08181387a43016de3312b5
Wojtas399 <wojtekp26@gmail.com>
2022-12-26 22:30:32 +0100

    Created static data in implementation of repository.

--- app/src/main/java/com/example/restcountriesapp/data/CountryRepositoryImpl.kt
+++ app/src/main/java/com/example/restcountriesapp/data/CountryRepositoryImpl.kt
@@ -3,9 +3,35 @@ package com.example.restcountriesapp.data
 import com.example.restcountriesapp.domain.Country
 import com.example.restcountriesapp.domain.CountryRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class CountryRepositoryImpl : CountryRepository {
   override fun getAllCountries(): Flow<List<Country>> {
-    TODO("Not yet implemented")
+    return flow {
+      emit(
+        listOf(
+          Country(
+            name = "Country 1",
+            capital = "Capital 1",
+            flag = "🏳🇦🇹",
+          ),
+          Country(
+            name = "Country 2",
+            capital = "Capital 2",
+            flag = "🏳️‍⚧️",
+          ),
+          Country(
+            name = "Country 3",
+            capital = "Capital 3",
+            flag = "🇨🇬",
+          ),
+          Country(
+            name = "Country 4",
+            capital = "Capital 4",
+            flag = "🇨🇳",
+          ),
+        ),
+      )
+    }
   }
 }
